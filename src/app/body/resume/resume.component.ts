6b8bc6c36004879dfb041456a674a687576c6524
hngyk <yiiyok@gmail.com>
2018-09-30 19:31:46 +0700

    5%

--- src/app/body/resume/resume.component.ts
+++ src/app/body/resume/resume.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent implements OnInit {
-
+tab = [null,null,null];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onShowDetail(no) {
+  if(this.tab[no] !== no){
+   this.tab[no] = no;
+  } else {
+    this.tab[no] = null;
+  }
   }
 
 }
