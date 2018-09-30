6ae4496ae7435a5146a5ecae6c57e7834df063d4
hngyk <yiiyok@gmail.com>
2018-09-30 14:30:35 +0700

    1st

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './header/header.component';
+import { ResumeComponent } from './body/resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    HeaderComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule
